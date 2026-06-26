7f2c87a162254ec01ab7a5c0e5cbc2c3d143f2f6
mangodreyy <audreyleong78@gmail.com>
2026-06-26 12:01:42 +0800

    Create redis.ts

---
+++ lib/redis.ts
@@ -0,0 +1,19 @@
+import { createClient } from "redis";
+
+const client = createClient({
+  url: process.env.REDIS_URL,
+});
+
+client.on("error", (err) => {
+  console.error("Redis Error:", err);
+});
+
+let connected = false;
+
+export async function getRedis() {
+  if (!connected) {
+    await client.connect();
+    connected = true;
+  }
+  return client;
+}
